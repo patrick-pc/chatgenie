fea0b0d2150a67806991cf3b4459615a26ee1294
spidΞy <web3slinger@gmail.com>
2022-12-29 18:23:30 -0600

    fix: change blurry icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="flex w-full items-center justify-between p-6">
-      <img className="h-8 cursor-pointer" src="/img/logo-160x160.svg" />
+      <img className="h-8 cursor-pointer" src="/img/logo-256x256.png" />
     </div>
   )
 }
