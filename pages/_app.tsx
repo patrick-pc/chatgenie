2cdd7a639ffaf56a18064477f008546ec07ad10f
spidΞy <web3slinger@gmail.com>
2023-01-01 20:56:37 -0600

    wip: add supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import '../styles/globals.css'
 import { CharactersProvider } from '../contexts/characters'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { Session, SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session
+}>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
   return (
-    <CharactersProvider>
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <Component {...pageProps} />
-    </CharactersProvider>
+    </SessionContextProvider>
   )
 }
---
+++ pages/signin.tsx
@@ -0,0 +1,64 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useState } from 'react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+
+export default function Login() {
+  const supabase = useSupabaseClient()
+  const [email, setEmail] = useState('')
+  const [submitted, setSubmitted] = useState(false)
+
+  const signIn = async () => {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+    })
+
+    if (error) {
+      console.log({ error })
+    } else {
+      const { error } = await supabase.auth.updateUser({ email })
+
+      if (error) console.log({ error })
+      setSubmitted(true)
+    }
+  }
+
+  if (submitted) {
+    return (
+      <div>
+        <h1>Please check your email to sign in</h1>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+      <input
+        className="w-full max-w-sm rounded-lg border-2 border-[#313335] bg-transparent py-2 px-4 text-sm placeholder-zinc-500 focus:border-indigo-400 focus:outline-none"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button
+        className="w-full max-w-sm rounded-lg bg-[#313335] px-4 py-2 transition hover:bg-[#414345] disabled:bg-[#313335] disabled:opacity-50"
+        onClick={() => signIn()}
+      >
+        Sign In
+      </button>
+    </div>
+  )
+}
+
+export const getServerSideProps = async (ctx) => {
+  const supabase = createServerSupabaseClient(ctx)
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (user)
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+
+  return { props: { user } }
+}
