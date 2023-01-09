71259c9b92e12659d67d5e526da8b48346fc4603
spidΞy <web3slinger@gmail.com>
2023-01-08 19:02:31 -0600

    wip: add profile page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+import { useUser } from '@supabase/auth-helpers-react'
+
 const Navbar = () => {
+  const user = useUser()
+
   return (
     <div className="flex w-full items-center justify-between p-6">
-      <img className="h-8 cursor-pointer" src="/img/logo-256x256.png" />
+      <Link href="/">
+        <img className="h-8 cursor-pointer" src="/img/logo-256x256.png" />
+      </Link>
+      {user ? (
+        <Link href="/profile">
+          <button>Profile</button>
+        </Link>
+      ) : (
+        <Link href="/signin">
+          <button>Sign In</button>
+        </Link>
+      )}
     </div>
   )
 }
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -2,12 +2,15 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { useState } from 'react'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
-export default function Login() {
+export default function SignIn() {
   const supabase = useSupabaseClient()
+
   const [email, setEmail] = useState('')
   const [submitted, setSubmitted] = useState(false)
 
   const signIn = async () => {
+    if (!email) return
+
     const { error } = await supabase.auth.signInWithOtp({
       email,
     })
@@ -39,28 +42,37 @@ export default function Login() {
 
   if (submitted) {
     return (
-      <div>
-        <h1>Please check your email to sign in</h1>
+      <div className="flex h-screen w-full flex-col items-center justify-center px-6">
+        <h4>Please check your email to sign in</h4>
       </div>
     )
   }
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-4 px-6">
-      <input
-        className="w-full max-w-sm rounded-lg border-2 border-[#313335] bg-transparent py-2 px-4 text-sm placeholder-zinc-500 focus:border-indigo-400 focus:outline-none"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      <div className="mb-16 flex w-full max-w-sm items-start">
+        <h4 className="text-4xl font-semibold">Sign In</h4>
+      </div>
+      <div className="flex w-full max-w-sm flex-col gap-1">
+        <label className="text-xs text-zinc-400">Email</label>
+        <input
+          className="w-full rounded-lg border-2 border-[#313335] bg-transparent py-2 px-4 text-sm placeholder-zinc-500 focus:border-indigo-400 focus:outline-none"
+          type="text"
+          placeholder="you@email.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
       <button
         className="w-full max-w-sm rounded-lg bg-[#313335] px-4 py-2 transition hover:bg-[#414345] disabled:bg-[#313335] disabled:opacity-50"
-        onClick={() => signIn()}
+        onClick={signIn}
+        disabled={!email}
       >
         Sign In
       </button>
       <p className="my-2">OR</p>
       <button
         className="w-full max-w-sm rounded-lg bg-[#313335] px-4 py-2 transition hover:bg-[#414345] disabled:bg-[#313335] disabled:opacity-50"
-        onClick={() => signInWithGoogle()}
+        onClick={signInWithGoogle}
       >
         Sign In With Google
       </button>
@@ -74,13 +86,14 @@ export const getServerSideProps = async (ctx) => {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (user)
+  if (user) {
     return {
       redirect: {
-        destination: '/',
+        destination: '/profile',
         permanent: false,
       },
     }
+  }
 
   return { props: { user } }
 }
