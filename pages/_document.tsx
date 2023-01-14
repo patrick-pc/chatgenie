cd77272468668eebaa8639706353a31599428a30
spidΞy <web3slinger@gmail.com>
2023-01-13 19:41:59 -0600

    patch: update og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,12 +8,12 @@ export default function Document() {
       <meta property="og:title" content="ChatGenie" />
       <meta property="og:description" content="Exploring the future of Human-AI conversation." />
       <meta property="og:site_name" content="ChatGenie" />
-      <meta property="og:image" content="https://chatgenie.xyz/img/og-image.png" />
+      <meta property="og:image" content="https://chatgenie.xyz/img/og.png" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="ChatGenie" />
       <meta name="twitter:description" content="Exploring the future of Human-AI conversation." />
-      <meta name="twitter:image" content="https://chatgenie.xyz/img/og-image.png" />
+      <meta name="twitter:image" content="https://chatgenie.xyz/img/og.png" />
 
       <meta name="theme-color" content="#131517" />
 
